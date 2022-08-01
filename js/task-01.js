e5a6d3901a2c7f779275cecebdebedd6d7f3d507
Kateryna Berehovych <kasia-94@ukr.net>
2022-08-01 22:55:17 +0300

    Update

--- js/task-01.js
+++ js/task-01.js
@@ -1,14 +1,7 @@
-const itemList = document.querySelector("#categories");
-console.log(`Number of categories:`, itemList.children.length);
+const itemList = document.querySelectorAll(".item");
+console.log(`Number of categories:`, itemList.length);
 
-const articleName = document.querySelectorAll(".item h2");
-const articleEl = document.querySelectorAll(".item ul");
-
-console.log(`Category:`, articleName[0].textContent);
-console.log(`Elements:`, articleEl[0].children.length);
-
-console.log(`Category:`, articleName[1].textContent);
-console.log(`Elements:`, articleEl[1].children.length);
-
-console.log(`Category:`, articleName[2].textContent);
-console.log(`Elements:`, articleEl[2].children.length);
+itemList.forEach((item) => {
+  console.log(`Category: ${item.querySelector("h2").textContent}`);
+  console.log(`Elements: ${item.querySelectorAll("li").length}`);
+});
