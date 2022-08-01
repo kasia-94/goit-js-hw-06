bda7a44d5579514d1231291e3b2dd5cf6ae2da63
Kateryna Berehovych <kasia-94@ukr.net>
2022-08-01 19:24:56 +0300

    Task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsContainer = document.querySelector("#ingredients");
+const ingredientsList = ingredients.map((ingredient) => {
+  const ingredientEl = document.createElement("li");
+  ingredientEl.classList.add("item");
+  ingredientEl.textContent = ingredient;
+
+  return ingredientEl;
+});
+
+console.log(ingredients);
+ingredientsContainer.append(...ingredientsList);
