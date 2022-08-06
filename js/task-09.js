6e0421526e18d647c9934f6943b29fd080e7fb0a
Kateryna Berehovych <kasia-94@ukr.net>
2022-08-06 09:43:23 +0300

    tasks  9-10

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,11 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnChangeColor = document.querySelector(".change-color");
+const changeColor = document.querySelector(".color");
+
+btnChangeColor.addEventListener("click", (event) => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  changeColor.textContent = getRandomHexColor();
+});
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,29 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnCreate = document.querySelector("button[data-create]");
+const btnDestroy = document.querySelector("button[data-destroy]");
+const input = document.querySelector("input");
+const boxes = document.querySelector("#boxes");
+
+btnCreate.addEventListener("click", createBoxes);
+btnDestroy.addEventListener("click", destroyBoxes);
+
+function createBoxes(amount) {
+  amount = input.value;
+  let basicSize = 30;
+  for (let i = 0; i < amount; i += 1) {
+    let boxElement = document.createElement("div");
+    let sizeBox = basicSize + 10;
+    boxElement.style.width = String(sizeBox) + "px";
+    boxElement.style.height = String(sizeBox) + "px";
+    boxElement.style.backgroundColor = getRandomHexColor();
+    boxes.append(boxElement);
+  }
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = "";
+  input.value = "";
+}
