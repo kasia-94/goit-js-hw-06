94d928250911f8acf7e0ad3f14d5213db8a80923
Kateryna Berehovych <kasia-94@ukr.net>
2022-08-06 16:13:39 +0300

    1

--- js/task-10.js
+++ js/task-10.js
@@ -14,11 +14,11 @@ function createBoxes(amount) {
   amount = input.value;
   for (let i = 0; i < amount; i += 1) {
     let boxElement = document.createElement("div");
+    boxes.append(boxElement);
     let sizeBox = 30 + (boxes.children.length - 1) * 10;
     boxElement.style.width = String(sizeBox) + "px";
     boxElement.style.height = String(sizeBox) + "px";
     boxElement.style.backgroundColor = getRandomHexColor();
-    boxes.append(boxElement);
   }
 }
 
